f79c2b039d8c79369db91794c28516fe8db8443e
Tahamuhommad <muhommadtaha52@gmail.com>
2024-04-22 16:03:58 +0500

    Initial commit

---
+++ tsc/number.ts
@@ -0,0 +1,22 @@
+
+
+import inquirer from 'inquirer'
+// computer will generate a number
+
+const randomNumber =Math.floor(Math.random() *10 + 1);
+
+
+const answer = await inquirer.prompt([
+{
+name :"userGuessedNumber",
+type:'number',
+message:"please guess a number 1-10:", 
+},
+]);
+if(answer.userGuessedNumber === randomNumber){
+    console.log("congratulations! you guessed right number")
+ 
+}else{
+    console.log('you guessed wrong number')
+}
+
